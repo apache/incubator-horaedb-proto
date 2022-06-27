be84f9b81255c759589a4b535a6b7abaee053ded
chunshao.rcs <chunshao.rcs@antgroup.com>
2022-06-27 20:09:56 +0800

    feat: modify go package

--- gen-go.sh
+++ gen-go.sh
@@ -4,4 +4,4 @@ set -ex
 
 protoc --proto_path=protos --go_out=. --go-grpc_out=. \
     ./protos/common.proto  ./protos/prometheus.proto \
-    ./protos/meta_cluster.proto ./protos/meta_service.proto ./protos/meta_storage.proto
+    ./protos/meta_cluster.proto ./protos/meta_service.proto ./protos/meta_storage.proto ./protos/storage.proto
