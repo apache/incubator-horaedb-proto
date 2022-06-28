99a382aead69d6929fc8af32bc8560df524ffe0e
chunshao.rcs <chunshao.rcs@antgroup.com>
2022-06-28 17:42:41 +0800

    fix: fix go compile

--- gen-go.sh
+++ gen-go.sh
@@ -2,6 +2,11 @@
 
 set -ex
 
+GO_PREFIX_PATH=github.com/CeresDB/ceresdbproto
+
 protoc --proto_path=protos --go_out=. --go-grpc_out=. \
     ./protos/common.proto  ./protos/prometheus.proto \
     ./protos/meta/* ./protos/storage.proto
+
+rm -rf pkg && mv $GO_PREFIX_PATH/pkg .
+rm -rf github.com
