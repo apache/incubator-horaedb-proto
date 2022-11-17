0910fe20e45b5b6fa269cf5621a6af5681e145ed
ShiKaiWi <xykwei@gmail.com>
2022-11-17 22:56:18 +0800

    feat: auto download protoc

--- rust/build.rs
+++ rust/build.rs
@@ -1,6 +1,10 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Download the protoc and set the path for tonic_build.
+    let protoc_path = protoc_bin_vendored::protoc_bin_path().unwrap();
+    std::env::set_var("PROTOC", protoc_path.as_os_str());
+
     tonic_build::configure().out_dir("./src").compile(
         &[
             "../protos/cluster.proto",
@@ -11,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "../protos/prometheus.proto",
             "../protos/storage.proto",
         ],
-        &["../protos"]
+        &["../protos"],
     )?;
     Ok(())
 }
