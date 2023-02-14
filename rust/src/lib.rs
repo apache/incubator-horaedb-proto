81a6d9ead104b2910f5c4484135054d51095090b
chunshao.rcs <chunshao.rcs@antgroup.com>
2023-02-14 13:53:59 +0800

    feat: add ceresdb engine proto (#49)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -9,3 +9,14 @@ pub mod meta_service;
 pub mod meta_storage;
 pub mod prometheus;
 pub mod storage;
+
+// engine
+pub mod manifest;
+pub mod oss_cache;
+pub mod remote_engine;
+pub mod schema;
+pub mod sst;
+pub mod sys_catalog;
+pub mod table_requests;
+pub mod time_range;
+pub mod wal_on_mq;
