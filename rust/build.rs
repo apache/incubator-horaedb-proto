874c015cd25d4cc6370499eec72e81fc7a432ca8
kamille <caoruiqiu.crq@antgroup.com>
2023-02-21 09:34:27 +0800

    refactor the build scripts.

--- rust/build.rs
+++ rust/build.rs
@@ -1,31 +1,79 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Download the protoc and set the path for tonic_build.
-    let protoc_path = protoc_bin_vendored::protoc_bin_path().map_err(|e| Box::new(e))?;
-    std::env::set_var("PROTOC", protoc_path.as_os_str());
+use std::{io::Write, path::Path};
+use walkdir::WalkDir;
 
-    tonic_build::configure().out_dir("./src").compile(
-        &[
-            "../protos/cluster.proto",
-            "../protos/common.proto",
-            "../protos/meta_event.proto",
-            "../protos/meta_service.proto",
-            "../protos/meta_storage.proto",
-            "../protos/prometheus.proto",
-            "../protos/storage.proto",
-            // engine
-            "../protos/engine/schema.proto",
-            "../protos/engine/manifest.proto",
-            "../protos/engine/oss_cache.proto",
-            "../protos/engine/remote_engine.proto",
-            "../protos/engine/sst.proto",
-            "../protos/engine/sys_catalog.proto",
-            "../protos/engine/table_requests.proto",
-            "../protos/engine/time_range.proto",
-            "../protos/engine/wal_on_mq.proto",
-        ],
-        &["../protos"],
-    )?;
-    Ok(())
+struct Builder {
+    out_dir: String,
+    proto_dir: String,
+    proto_paths: Vec<String>,
+    module_names: Vec<String>,
+}
+
+impl Builder {
+    fn new(out_dir: String, proto_dir: String) -> Result<Self, Box<dyn std::error::Error>> {
+        // Get each proto's path and related module name.
+        let proto_dir = Path::new(proto_dir.as_str()).to_str().unwrap().to_owned();
+
+        let mut proto_paths = Vec::new();
+        let mut module_names = Vec::new();
+        for entry in WalkDir::new(proto_dir.clone())
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|e| !e.file_type().is_dir() && !e.file_type().is_symlink())
+        {
+            proto_paths.push(entry.path().display().to_string());
+
+            let module_name = entry
+                .file_name()
+                .to_str()
+                .unwrap()
+                .split(".")
+                .into_iter()
+                .next()
+                .unwrap()
+                .to_owned();
+            module_names.push(module_name);
+        }
+
+        Ok(Self {
+            out_dir,
+            proto_dir,
+            proto_paths,
+            module_names,
+        })
+    }
+
+    fn generate(self) -> Result<(), Box<dyn std::error::Error>> {
+        self.generate_files()?.generate_mod_file()?;
+
+        Ok(())
+    }
+
+    fn generate_files(self) -> Result<Self, Box<dyn std::error::Error>> {
+        // Compile proto files.
+        tonic_build::configure()
+            .compile(&self.proto_paths, &[self.proto_dir.clone()])
+            .map_err(|e| Box::new(e))?;
+
+        Ok(self)
+    }
+
+    fn generate_mod_file(self) -> Result<Self, Box<dyn std::error::Error>> {
+        // Generate mod.rs file.
+        let mut mod_file = std::fs::File::create(format!("{}/mod.rs", self.out_dir))?;
+        for module_name in self.module_names.iter() {
+            write!(mod_file, "pub mod {};\n", module_name)?;
+        }
+
+        Ok(self)
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let out_dir = std::env::var("OUT_DIR").map_err(|e| Box::new(e))?;
+
+    let protos_dir = "protos".to_string();
+    let builder = Builder::new(out_dir, protos_dir)?;
+    builder.generate()
 }
