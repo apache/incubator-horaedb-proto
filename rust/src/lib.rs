f8fd35756fba56bbbfd346ac1f6dcf21bee6aa10
kamille <34352236+Rachelint@users.noreply.github.com>
2023-02-22 17:29:46 +0800

    feat: refactor build script for publishing (#59)
    * refactor the build scripts.
    
    * modify Cargo.toml for publishing.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,21 +2,10 @@
 
 //! Protobuf messages
 
-pub mod cluster;
-pub mod common;
-pub mod meta_event;
-pub mod meta_service;
-pub mod meta_storage;
-pub mod prometheus;
-pub mod storage;
+pub use protos::*;
 
-// engine
-pub mod manifest;
-pub mod oss_cache;
-pub mod remote_engine;
-pub mod schema;
-pub mod sst;
-pub mod sys_catalog;
-pub mod table_requests;
-pub mod time_range;
-pub mod wal_on_mq;
+#[allow(dead_code)]
+#[allow(clippy::all)]
+mod protos {
+    include!(concat!(env!("OUT_DIR"), "/mod.rs"));
+}
