df8b91eb809a53e3a6d15387ce9872d51ec4d5ff
kamille <caoruiqiu.crq@antgroup.com>
2023-02-28 15:38:15 +0800

    fix protoc download.

--- rust/build.rs
+++ rust/build.rs
@@ -71,6 +71,11 @@ impl Builder {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Download the protoc and set the path for tonic_build.
+    let protoc_path = protoc_bin_vendored::protoc_bin_path().map_err(|e| Box::new(e))?;
+    std::env::set_var("PROTOC", protoc_path.as_os_str());
+
+    // Build protos.
     let out_dir = std::env::var("OUT_DIR").map_err(|e| Box::new(e))?;
 
     let protos_dir = "protos".to_string();
